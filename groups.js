9dfa91c2ba6f989901c0ee5f7a68b114c158c0ef
Egor Spirin <spirin.egor@mail.ru>
2017-09-17 01:35:15 +0300

    connect with google docs

--- groups.js
+++ groups.js
@@ -1,14 +1,9 @@
 var currentGroup = 'A';
 
 function openGroup(groupName) {
-    var folder = 'group' + groupName;
     currentGroup = groupName;
-    var content = document.getElementById('content');
-    content.innerHTML = '';
-    jQuery.get(folder + '/training.html', function(data){
-        content.innerHTML = data;
-    });
     setGroupStyle();
+    openTraining();
 }
 
 function openMonitor() {
@@ -32,7 +27,13 @@ function openList() {
 }
 
 function openTraining() {
-    openGroup(currentGroup);
+    var folder = 'group' + currentGroup;
+    var content = document.getElementById('content');
+    content.innerHTML = '';
+    jQuery.get(folder + '/training.html', function(data){
+        content.innerHTML = data;
+    });
+    getTrainingPlan();
     setButtonStyle('training');
 }
 
@@ -60,4 +61,48 @@ function setGroupStyle() {
             groups[i].classList.remove('w3-text-teal');
         }
     }
+}
+
+const spreadsheet = {
+    'A': '1GgDJsBjx6z4-rdrQWHQBw2p0M3puKdn9uH0uGDFQ0xk',
+    'B': '1LHvu2ON0YBfg7SnesLhXFBl8ejQHRqM7-f80h-MILOI',
+    'C': '1cDhgxkuxYxn9fivzbt8Sm6VV-Pw3bH6mBrxDDWVOQOA',
+    'Teams': '16-uaTSzXq22uV1jyjGzHixIFNvpAQpdBg1VLqheYsxg'
+};
+
+function generateCard(title, description, link, date) {
+    var template = `
+        <div class="w3-card-2 w3-white training">
+            <div class="card">
+                <div class="card-block text-right">
+                    <h4 class="card-title">${title}</h4>
+                    <p class="card-text">${description}</p>
+                    <p class="card-text"><b>${date}</b></p>
+                    ${title !== 'No contests yet' ? '<a href="#" class="w3-button w3-black margin-bottom">Enter contest</a>'
+                            : ''}
+                </div>
+            </div>
+        </div>
+    `;
+    return template;
+}
+
+function getTrainingPlan() {
+    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheet[currentGroup] + "/od6/public/values?alt=json";
+    $.getJSON(url, function(data) {
+        var entry = data.feed.entry;
+        if ($(entry).length === 0) {
+            var card = generateCard('No contests yet', '', '', '');
+            $('#training-plan').append(card); 
+        }
+        $(entry).each(function() {
+            var date = this.gsx$date.$t;
+            var title = this.gsx$title.$t;
+            var description = this.gsx$description.$t;
+            var link = this.gsx$link.$t;
+            var card = generateCard(title, description, link, date);
+            $('#training-plan').append(card);
+        });
+    });
+
 }
